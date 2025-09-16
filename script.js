0b06cbbb832018c345f37c4f7faaef1c6f4f2b74
David Karikari <davidkarikari89@gmail.com>
2025-09-16 18:13:57 +0000

    Made nav links responsive

--- script.js
+++ script.js
@@ -1,64 +1,74 @@
-const navbarItems = document.querySelectorAll(".navbar-item a");
-const themeBtn = document.querySelector(".theme-btn");
-
 const myProjectContainer = document.querySelectorAll(".my-projects");
 const myProjectTextBox = document.querySelectorAll(".project-text-box");
 
 myProjectContainer.forEach((el, idx) => {
-  if (idx % 2 === 0) {
-    el.classList.add("left-container");
-  } else {
-    el.classList.add("right-container");
-  }
+  el.classList.add(idx % 2 === 0 ? "left-container" : "right-container");
 });
 
 myProjectTextBox.forEach((el, idx) => {
   const span = document.createElement("span");
+  span.classList.add(
+    idx % 2 === 0 ? "left-container-arrow" : "right-container-arrow"
+  );
   el.appendChild(span);
-
-  if (idx % 2 === 0) {
-    span.classList.add("left-container-arrow");
-  } else {
-    span.classList.add("right-container-arrow");
-  }
 });
 
-function removeClassList(item) {
-  item.forEach((i) => {
-    i.classList.remove("active");
-  });
-}
-
-navbarItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    removeClassList(navbarItems);
-    item.classList.add("active");
-  });
-});
-
-themeBtn.addEventListener("click", () => {
-  document.querySelector("html").classList.toggle("dark");
-});
-
-// Swiper JS
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
-  speed: 5000, // bigger = slower, smoother scroll
+  speed: 5000,
   autoplay: {
-    delay: 0, // no pause between transitions
+    delay: 0,
     disableOnInteraction: false,
     pauseOnMouseEnter: false,
   },
-  slidesPerView: "auto", // each slide takes its natural width
-  spaceBetween: 30, // adjust spacing between logos
+  slidesPerView: "auto",
+  spaceBetween: 30,
   freeMode: {
     enabled: true,
     momentum: false,
   },
-
-  // Optional: remove pagination/navigation if not needed
   pagination: false,
   navigation: false,
   scrollbar: false,
+});
+
+const navLinks = document.querySelectorAll(".navbar a");
+
+function clearActive() {
+  navLinks.forEach((link) => link.classList.remove("active"));
+}
+
+navLinks.forEach((el) => {
+  el.addEventListener("click", () => {
+    clearActive();
+    el.classList.add("active");
+  });
+});
+
+const sections = document.querySelectorAll("section[id]");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry);
+        navLinks.forEach((el) => {
+          if (
+            el.getAttribute("href") === `#${entry.target.getAttribute("id")}`
+          ) {
+            clearActive();
+            el.classList.add("active");
+          }
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
